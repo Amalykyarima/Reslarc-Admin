0147030e7e5306d627ed8fd76427d90683427392
Amalykyarima <91787678+Amalykyarima@users.noreply.github.com>
2024-11-21 21:58:37 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const app = express();
 app.use(compression());
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/coreui-free-angular-admin-template'));
+app.use(express.static(__dirname + '/dist/coreui-free-angular-admin-template/browser'));
 
 // Send all requests to index.html
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/coreui-free-angular-admin-template/index.html'));
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname + '/dist/coreui-free-angular-admin-template/browser/index.html'));
 });
 
 // default PORT
